9a7fcd8e7da62b0dd71a83e6fa72b8d679680571
Mitch Downey <mitchldowney@gmail.com>
2020-03-09 03:59:57 -0500

    Update setup.js

--- podverse-web/setup.js
+++ podverse-web/setup.js
@@ -9,10 +9,14 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
 
 module.exports = async function() {
   console.log(chalk.green('Setup Puppeteer'))
-  const browser = await puppeteer.launch({
+
+  const launchSettings = {
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
-  })
+  }
+  console.log(chalk.green(launchSettings))
+
+  const browser = await puppeteer.launch(launchSettings)
   // This global is not available inside tests but only in global teardown
   global.__BROWSER_GLOBAL__ = browser
   // Instead, we expose the connection details via file system to be used in tests
