f1a414d71544480a15ed65741f32acae647a977a
capntechbeard <capntechbeard@gmail.com>
2020-03-18 18:03:14 -0500

    Update smoketests.js

--- podverse-web/__tests__/smoketests.js
+++ podverse-web/__tests__/smoketests.js
@@ -19,62 +19,47 @@ describe(
             '/ (Pages)',
             () => {
 
-                it.only('should load homepage without error', async () => {
+                it('should load homepage without error', async () => {
                     await page.goto(origin)
                     await page.waitForXPath("//a[contains(text(), 'Podverse')]")
-                }, 25000)
+                }, 35000)
 
-                it.only('should load About page when About footer link is pressed', async () => {
+                it('should load About page when About footer link is pressed', async () => {
                     await page.click('.footer-bottom__link[href="/about"]')
                     await page.waitForXPath("//h3[contains(text(), 'About')]")
                 }, 10000)
 
                 it('should load Terms page when Terms footer link is pressed', async () => {
                     await page.click('.footer-bottom__link[href="/terms"]')
-                    await page.waitForSelector('h3', {
-                        visible: true,
-                        text: "Terms of Service"
-                    })
+                    await page.waitForXPath("//h3[contains(text(), 'Terms of Service')]")
                 }, 10000)
 
                 it('should load Premium page when Premium footer link is pressed', async () => {
                     await page.click('.footer-bottom__link[href="/membership"]')
-                    await page.waitForSelector('h3', {
-                        visible: true,
-                        text: "Premium"
-                    })
+                    await page.waitForXPath("//h3[contains(text(), 'Premium')]")
                 }, 10000)
 
                 it('should load Search page when search magnifying glass link is pressed', async () => {
-                    await page.click('.fa-search[href="/search"]')
-                    await page.waitForSelector('h3', {
-                        visible: true,
-                        text: "Search"
-                    })
+                    await page.click('.nav-link[href="/search"]')
+                    await page.waitForXPath("//h3[contains(text(), 'Search')]")
                 }, 10000)
 
                 it('should load Podcasts page when Podcasts header link is pressed', async () => {
                     await page.click('.nav-link[href="/podcasts"]')
-                    await page.waitForSelector('h3', {
-                        visible: true,
-                        text: "Podcasts"
-                    })
+                    await page.waitForXPath("//h3[contains(text(), 'Podcasts')]")
                 }, 10000)
 
                 it('should load Playlist page when Playlist header link is pressed', async () => {
                     await page.click('.nav-link[href="/playlists"]')
-                    await page.waitForSelector('div', {
-                        visible: true,
-                        text: "Login to view your playlists"
-                    })
+                    await page.waitForXPath("//div[contains(text(), 'Login to view your playlists')]")
                 }, 10000)
 
-                it.only('should load Settings page when dropdown is clicked and Settings link is pressed', async () => {
+                it('should load Settings page when dropdown is clicked and Settings link is pressed', async () => {
                     await page.click('.dropdown-toggle')
                     await page.click('.dropdown-item[href="/settings"]')
                     await page.waitForXPath("//h3[contains(text(), 'Interface')]")
                 }, 10000)
-            }, 30000)
+            }, 60000)
     },
     20000
 )
