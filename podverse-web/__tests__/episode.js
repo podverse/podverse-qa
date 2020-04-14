653dc62944ac0bce15776cc4c5dc0501259892fc
capntechbeard <capntechbeard@gmail.com>
2020-04-14 00:57:26 -0500

    updated 'episode.js' and 'home.js'

--- podverse-web/__tests__/episode.js
+++ podverse-web/__tests__/episode.js
@@ -1,4 +1,4 @@
-const { getTestOrigin } = require('../utility')
+const { getTestOrigin, scrollIntoView } = require('../utility')
 const origin = getTestOrigin()
 
 describe(
@@ -19,5 +19,96 @@ describe(
         it('loads the page', async () => {
             await page.waitForXPath('//div[contains(text(), "The Americans are coming, but will the war be over by the time they get there? Germany throws everything into a last series of stupendous attacks in the West while hoping to avoid getting burned by a fire in the East they helped fan.")]')
         }, 10000)
+
+        it('Episode Page Dropdowns: Clips ► Episodes', async () => {
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Clips")]')
+            await elements1[0].click();
+            await scrollIntoView(page, '.media-list__selects')
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Episodes")]')
+            await elements2[0].click();
+            await page.waitFor(2000)
+            await page.waitForXPath('//div[contains(text(), "Show 53 - Blueprint for Armageddon IV")]')
+        }, 10000)
+
+        it('Episode Page Dropdowns: Episodes ► Clips', async () => {
+            await page.waitFor(2000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Episodes")]')
+            await elements1[0].click();
+            await page.waitFor(2000)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Clips")]')
+            await elements2[0].click();
+            await page.waitForXPath('//div[contains(text(), "Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae.")]')
+        }, 12000)
+
+        it('Episode Page: Sort ► Most Recent', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "top - past week")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "most recent")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'most recent')]")
+        })
+
+        it('Episode Page: Sort ► Top Past Day', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "most recent")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "top - past day")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'top - past day')]")
+        })
+
+        it('Episode Page: Sort ► Top Past Week', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "top - past day")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "top - past week")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'top - past week')]")
+        })
+
+        it('Episode Page: Sort ► Top Past Month', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "top - past week")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "top - past month")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'top - past month')]")
+        })
+
+        it('Episode Page: Sort ► Top Past Year', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "top - past month")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "top - past year")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'top - past year')]")
+        })
+
+        it('Episode Page: Sort ► Top All Time', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "top - past year")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "top - all time")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'top - all time')]")
+        })
+
+        it('Episode Page: Sort ► Random', async () => {
+            await scrollIntoView(page, '.media-list__selects')
+            await page.waitFor(1000)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "top - all time")]')
+            await elements1[0].click();
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "random")]')
+            await elements2[0].click();
+            await page.waitForXPath("//button[contains(text(), 'random')]")
+        })
 
     }, 60000)
--- podverse-web/__tests__/home.js
+++ podverse-web/__tests__/home.js
@@ -1,4 +1,4 @@
-const { getTestOrigin } = require('../utility')
+const { getTestOrigin, scrollIntoView } = require('../utility')
 const origin = getTestOrigin()
 
 describe(
@@ -20,5 +20,153 @@ describe(
             await page.waitForXPath('//div[contains(text(), "Amet aliquam id diam maecenas ultricies mi eget.")]')
             await page.waitForXPath('//div[contains(text(), "Magna fringilla urna porttitor rhoncus dolor.")]')
         }, 10000)
+
+        it('Podcast Categories: Subscribed', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "All Podcasts")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Subscribed")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Subscribed")]')
+        }, 10000)
+
+        it('Podcast Categories: All Podcasts', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Subscribed")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "All Podcasts")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "All Podcasts")]')
+        }, 10000)
+
+        it('Podcast Categories: Arts', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "All Podcasts")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Arts")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Arts")]')
+        }, 10000)
+
+        it('Podcast Categories: Business', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Arts")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Business")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Business")]')
+        }, 10000)
+
+        it('Podcast Categories: Comedy', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Business")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Comedy")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Comedy")]')
+        }, 10000)
+
+        it('Podcast Categories: Education', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Comedy")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Education")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Education")]')
+        }, 10000)
+
+        it('Podcast Categories: Games & Hobbies', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Education")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Games & Hobbies")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Games & Hobbies")]')
+        }, 10000)
+
+        it('Podcast Categories: Government & Organizations', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Games & Hobbies")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Government & Organizations")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Government & Organizations")]')
+        }, 10000)
+
+        it('Podcast Categories: Health', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Government & Organizations")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Health")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Health")]')
+        }, 10000)
+
+        it('Podcast Categories: Music', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Health")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Music")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Music")]')
+        }, 10000)
+
+        it('Podcast Categories: News & Politics', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Music")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "News & Politics")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "News & Politics")]')
+        }, 10000)
+
+        it('Podcast Categories: Religion & Spirituality', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "News & Politics")]')
+            await elements1[0].click();
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Religion & Spirituality")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Religion & Spirituality")]')
+        }, 10000)
+
+        it('Podcast Categories: Science & Medicine', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Religion & Spirituality")]')
+            await elements1[0].click();
+            await page.waitFor(2000)
+            await scrollIntoView(page, '.pv-divider')
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Science & Medicine")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Science & Medicine")]')
+        }, 10000)
+
+        it('Podcast Categories: Society & Culture', async () => {
+            await page.waitFor(100)
+            const elements1 = await page.$x('//button[@class="transparent dropdown-toggle btn btn-secondary"][contains (text(), "Science & Medicine")]')
+            await elements1[0].click();
+            await page.waitFor(2000)
+            await scrollIntoView(page, '.footer')
+            await page.waitFor(100)
+            const elements2 = await page.$x('//button[@class="dropdown-item"][contains (text(), "Society & Culture")]')
+            await elements2[0].click();
+            await page.waitForXPath('//button[contains(text(), "Society & Culture")]')
+        }, 10000)
+
+
+
+
 
     }, 60000)
