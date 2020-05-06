c804a3560b0932816b3a2637d3f134826da83bf0
Mitch Downey <mitchldowney@gmail.com>
2020-05-06 02:47:53 -0500

    Update moveToXpath.js

--- podverse-web/extensions/moveToXpath.js
+++ podverse-web/extensions/moveToXpath.js
@@ -1,5 +1,4 @@
 module.exports.command = function (selector, xoffset = 0, yoffset = 0) {
-  console.log('hi', selector, xoffset, yoffset)
   this.moveToElement('xpath', selector, xoffset, yoffset)
 
   return this
