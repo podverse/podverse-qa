9a39c2f18ca71d2e836247f793cf1200a98aa690
Mitch Downey <mitchldowney@gmail.com>
2025-10-05 22:27:01 -0500

    fix: reduce max podcasts fetched to 30 for improved performance

--- src/faker/create/feeds-channels-items.ts
+++ src/faker/create/feeds-channels-items.ts
@@ -2,7 +2,7 @@ import { parseRSSFeedAndSaveToDatabase } from 'podverse-parser';
 import { podcastIndexService } from '../../factories/podcastIndexService';
 
 export default async function createFeedsChannelsItems() {
-  const max = 50;
+  const max = 30;
   const { feeds } = await podcastIndexService.trendingGetPodcasts(max);
 
   for (const feed of feeds) {
